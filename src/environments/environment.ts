c4405c85af9c70271e6f3b6865e62cce31a15d00
Crhistian Ramirez <16483662+crhistianramirez@users.noreply.github.com>
2019-09-04 12:36:15 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   clientID: 'C9D7D4C2-3F5F-4763-BC6B-77C5D978A2F5',
-  buyerID: 'initBuyer',
+  buyerID: 'allchalkedup',
   buyerClientID: '9F6E0D67-529F-4460-8236-30B375A025F0',
   buyerUrl: 'localhost:1000',
   middlewareUrl: 'my-middleware-url.com/api',
